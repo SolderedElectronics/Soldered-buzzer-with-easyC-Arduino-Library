cf24769b6a9b9ebc8c7308e1c297869c28b9a3fd
Main Push Robot <robot@example.com>
2023-01-02 08:02:02 +0000

    Committing dev changes

---
+++ examples/BasicBuzzer/BasicBuzzer.ino
@@ -0,0 +1,39 @@
+/**
+ **************************************************
+ *
+ * @file        BasicBuzzer.ino
+ * @brief       Example for the buzzer board with easyC.
+ * 
+ *              For this example you will need Dasduino, easyC cable, and buzzer with easyC board.
+ *              Connect the buzzer board with easyC to Dasduino and upload this code.
+ *              The buzzer will turn on and off every 200ms.
+ * 
+ * @link        solde.red/333186
+ *
+ * @authors     Karlo Leksic for soldered.com
+ ***************************************************/
+
+#include "Buzzer-easyC-SOLDERED.h"
+
+Buzzer buzzer;
+
+void setup()
+{
+    // Initialize buzzer
+    buzzer.begin();
+
+    // Set the buzzer volume
+    buzzer.setVolume(1);
+}
+
+void loop()
+{
+    // Turn on the buzzer with the preset volume
+    // It's also possible to turn on the buzzer with the buzzer.on(volume); The volume is from 0 to 255
+    buzzer.on();
+    delay(100);
+
+    // Turn off the buzzer
+    buzzer.off();
+    delay(100);
+}
---
+++ src/Buzzer-easyC-SOLDERED.cpp
@@ -0,0 +1,78 @@
+/**
+ **************************************************
+ *
+ * @file        Buzzer-easyC-SOLDERED.cpp
+ * @brief       Functions for buzzer with easyC board.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Karlo Leksic for soldered.com
+ ***************************************************/
+
+
+#include "Buzzer-easyC-SOLDERED.h"
+
+/**
+ * @brief                   Buzzer native constructor.
+ *
+ * @param int _pin          Pin on which the buzzer is connected.
+ */
+Buzzer::Buzzer(int _pin)
+{
+    pin = _pin;
+    native = 1;
+}
+
+/**
+ * @brief                   Constructor for buzzer with easyC.
+ */
+Buzzer::Buzzer()
+{
+    native = 0;
+}
+
+/**
+ * @brief                   Overloaded function for virtual in base class to initialize Buzzer specific.
+ */
+void Buzzer::initializeNative()
+{
+    pinMode(pin, OUTPUT);
+}
+
+/**
+ * @brief                   Turn off the buzzer.
+ */
+void Buzzer::off()
+{
+    byte data = 0;
+    sendData(&data, 1);
+}
+
+/**
+ * @brief                   Turn the buzzer on with the volume parameter.
+ * 
+ * @param byte _volume      The intensity of the buzzer sound from 0 to 255.                   
+ */
+void Buzzer::on(byte _volume)
+{
+    volume = _volume;
+    sendData(&volume, 1);
+}
+
+/**
+ * @brief                   Turn on the buzer with the preset volume.
+ */
+void Buzzer::on()
+{
+    sendData(&volume, 1);
+}
+
+/**
+ * @brief                   Set buzzer volume.
+ * 
+ * @param byte _volume      Buzzer volume from 0 to 255.
+ */
+void Buzzer::setVolume(byte _volume)
+{
+    volume = _volume;
+}
